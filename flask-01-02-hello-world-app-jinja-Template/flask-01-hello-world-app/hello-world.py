8d0186cb8f4319a257069d7f0da4aa559a2d9ff9
bbilgec <bekirbilgec42@gmail.com>
2022-08-15 15:54:15 +0300

    "part1 and part2"

--- flask-01-02-hello-world-app-jinja-Template/flask-01-hello-world-app/hello-world.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-01-hello-world-app/hello-world.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello'
+
+@app.route('/second')
+def second():
+    return 'Hello Penguins'
+
+@app.route('/third/subthird')
+def third():
+    return 'Hello subthird'    
+
+
+@app.route('/forth/<float:id>')
+def forth(id):
+    return f'merhaba{id}'
+
 
 
 if __name__ == '__main__':
