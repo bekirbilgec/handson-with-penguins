424d0f06c01d98770e9ea555df4e908cffc16a26
bbilgec <bekirbilgec42@gmail.com>
2022-08-15 16:23:45 +0300

    yeni kodlar eklendi

--- flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html", number1=34, number2=45)
 
 if __name__== "__main__":
     app.run(debug=True)
